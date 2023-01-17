bfa54e53011b1b16cc248bf7cd58e5f0efe15f97
EC2 Default User <ec2-user@ip-172-31-1-151.ap-northeast-1.compute.internal>
2023-01-17 11:42:11 +0000

    [Finish] bookers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_15_030553) do
+ActiveRecord::Schema.define(version: 2023_01_17_030553) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
